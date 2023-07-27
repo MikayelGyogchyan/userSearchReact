38a6b01c530fec4b260ffca45c7b3f378fdc51b3
DavitGyogchyan <mikayel1999@mail.com>
2023-07-27 22:51:03 +0000

    second commit,  Multi-Search Function

--- usersearch/src/App.jsx
+++ usersearch/src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Users } from "./users";
-import './App.css';
+import "./App.css";
+import Table from "./Table";
 
 // BASIC SEARCH
 
 function App() {
   const [query, setQuery] = useState("");
+
+  const keys = ["first_name", "last_name", "email"];
+
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
   return (
     <div className="App">
       <input
@@ -13,15 +22,7 @@ function App() {
         placeholder="Search..."
         onChange={(e) => setQuery(e.target.value.toLowerCase())}
       />
-      <ul className="list">
-        {Users.filter((asd) =>
-          asd.first_name.toLowerCase().includes(query)
-        ).map((user) => (
-          <li className="listItem" key={user.id}>
-            {user.first_name}
-          </li>
-        ))}
-      </ul>
+      <Table data={search(Users)} />
     </div>
   );
 }
